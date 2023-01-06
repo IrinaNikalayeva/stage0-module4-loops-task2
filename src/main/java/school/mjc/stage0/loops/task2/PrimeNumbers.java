940f6100900f738ba41dc0fe832370fb4ff32a4f
Iryna_Nikalayeva <irinnik92@gmail.com>
2023-01-06 22:24:47 +0100

    Performed PrimeNumbers task

--- src/main/java/school/mjc/stage0/loops/task2/PrimeNumbers.java
+++ src/main/java/school/mjc/stage0/loops/task2/PrimeNumbers.java
@@ -2,5 +2,15 @@ package school.mjc.stage0.loops.task2;
 
 public class PrimeNumbers {
     public void printPrimeNumbers(int printToInclusive) {
+        int num = 2;
+        while (num <= printToInclusive) {
+            if (num == 2 || num == 3 || num == 5) {
+                System.out.println(num);
+            }
+            if (num % 2 != 0 && num % 3 != 0 && num % 5 != 0) {
+                    System.out.println(num);
+                }
+            num++;
+        }
     }
 }
