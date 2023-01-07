426e3e044f77dd32282a2e0301122c672ba8c0b6
Iryna_Nikalayeva <irinnik92@gmail.com>
2023-01-07 19:48:20 +0100

    Performed PowerOfTwo task

--- src/main/java/school/mjc/stage0/loops/task2/PowerOfTwo.java
+++ src/main/java/school/mjc/stage0/loops/task2/PowerOfTwo.java
@@ -2,6 +2,14 @@ package school.mjc.stage0.loops.task2;
 
 public class PowerOfTwo {
     public void printPower(int power) {
-
+        int powerCounter = 0;
+        if (power >= 0) {
+            while (powerCounter <= power) {
+                System.out.println((int) Math.pow(2, powerCounter));
+                powerCounter++;
+            }
+        } else {
+            System.out.println("too much power");
+        }
     }
 }
