736d0a3036b5841e0ffa58fe6443c6914de5a71e
Iryna_Nikalayeva <irinnik92@gmail.com>
2023-01-07 19:44:34 +0100

    Performed FactorialNumbers task

--- src/main/java/school/mjc/stage0/loops/task2/FactorialNumbers.java
+++ src/main/java/school/mjc/stage0/loops/task2/FactorialNumbers.java
@@ -2,6 +2,15 @@ package school.mjc.stage0.loops.task2;
 
 public class FactorialNumbers {
     public void printFactorialRow(int printToInclusive) {
-
+        int counter = 0;
+        int num = 1;
+        while (counter <= printToInclusive) {
+            num = num * counter;
+            if (num == 0) {
+                num = 1;  // !0 == 1
+            }
+            System.out.println(num);
+            counter++;
+        }
     }
 }
